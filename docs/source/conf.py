a22e18d4f5ad2a627a410ac5637b1b57e4951aca
Lukas Voegtle <5764745+voegtlel@users.noreply.github.com>
2026-08-27 14:49:25 +0200

    Remove old docs quirks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,7 +137,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/custom.css"]
 
 # Favicon configuration
 html_favicon = "_static/favicon.ico"
